b5b01d193ff5a33ac6eb079649ae4beeb67d0603
Taylor Treece <taylor.treece@protonmail.com>
2021-03-04 19:25:09 -0600

    commit tree ce3eb4f194c4dd7c92ec3bf6eaf3659177860796

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Butter Bean"
+    end
+
+    get '/hometown' do 
+        "My hometown is Flavortown"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is WAP"
+    end
 end
